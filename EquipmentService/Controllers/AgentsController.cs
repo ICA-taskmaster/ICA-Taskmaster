f8b62a13b9d9159cf7fa9fe1b5bea8500b3bd5a4
Giang <463189@student.fontys.nl>
2023-05-09 11:26:35 +0200

    feat: add test equipment AgentsController.cs

---
+++ EquipmentService/Controllers/AgentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class AgentsController : ControllerBase {
+    public AgentsController() {
+        
+    }
+    
+    [HttpPost]
+    public ActionResult testInboundConnection() {
+        Console.WriteLine("received inbound connection from POST equipment service");
+        return Ok("inbound test of from agents controller");
+    }
+}
